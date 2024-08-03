cd0f286dadd701f2de939474cc106a8d508b13f9
jrest01 <julian11072000@hotmail.com>
2024-08-03 16:12:50 -0500

    Adding libraries

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Blazor___WebAssembly;
+using Blazored.Toast;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -8,6 +9,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 var apiUrl = builder.Configuration.GetValue<string>("apiURL");
 
+builder.Services.AddBlazoredToast();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(apiUrl) });
 // builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://dummyjson.com/c/d433-e088-49bd-8623") });
